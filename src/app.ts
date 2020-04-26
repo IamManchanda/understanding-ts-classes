1408208d1c35cf2770fe4efb4e015b7fd548c370
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-04-26 17:41:25 +0530

    TypeScript starting Practice in class!

---
+++ src/app.ts
@@ -0,0 +1,74 @@
+class Department {
+  private employees: string[] = [];
+
+  constructor(private readonly id: string, public name: string) {}
+
+  describe(this: Department) {
+    console.log(`Department (${this.id}): ${this.name}`);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+  }
+
+  addReport(report: string) {
+    this.reports.push(report);
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+class ITDepartment extends Department {
+  constructor(id: string, public admins: string[]) {
+    super(id, "IT");
+  }
+
+  printAdmins() {
+    console.log(this.admins);
+  }
+}
+
+const accounting = new AccountingDepartment("d1", []);
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+accounting.addEmployee("Anna");
+accounting.name = "Accounting & Finance";
+accounting.addReport("Something went wrong");
+
+console.log(accounting);
+accounting.describe();
+accounting.printEmployeeInformation();
+accounting.printReports();
+
+const infoTech = new ITDepartment("d2", ["Harry"]);
+infoTech.addEmployee("Harry");
+infoTech.addEmployee("Zuck");
+infoTech.addEmployee("Larry");
+infoTech.name = "Information Technology";
+
+console.log(infoTech);
+infoTech.describe();
+infoTech.printEmployeeInformation();
+infoTech.printAdmins();
+
+/* 
+const accountingCopy = {
+  name: "Accounting 2",
+  describe: accounting.describe,
+};
+
+accountingCopy.describe(); 
+*/
